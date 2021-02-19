1b2fe6997a7bb06f39c86c8dd81e38cbe2374041
anandkushwaha064 <68426503+anandkushwaha064@users.noreply.github.com>
2021-02-20 03:14:41 +0530

    Update TestSuitExcution.java

--- src/test/java/com/spotify/testsuits2/TestSuitExcution.java
+++ src/test/java/com/spotify/testsuits2/TestSuitExcution.java
@@ -22,12 +22,12 @@ public class TestSuitExcution {
 
 	@DataProvider(name = "testData",parallel = true)
 	public Object[][] testData() {
-		int test_start = 2;
+		int test_start = 5;
 		int test_end = 12;
 		int totaltests = test_end - test_start + 1;
 		Object[][] tests = new Object[totaltests][1];
-		for (int i = 0; i <= totaltests; i++) {
-			tests[i][0] = i;
+		for (int i = 0; i <totaltests; i++) {
+			tests[i][0] = test_start++;
 		}
 		return tests;
 	}
